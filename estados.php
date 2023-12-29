bc62b35c70f570f1f946377eb5b4bf7ba17fd8aa
João Corner <joaoeduardocorner@gmail.com>
2023-12-28 22:31:07 -0300

    alteração de button e input para o tipo form

--- estados.php
+++ estados.php
@@ -4,14 +4,15 @@ require_once('cabecalho.php');
 
 <div class="container" style="background: #f5f2f2; padding: 10px">
     <div class="row">
-        <div class="col-md-2">
-            <input type="text" class="form-control" id="nome" name="nome" placeholder="Sigla">
-        </div>
-        <div class="col-md-2">
-            <button class="btn btn-primary">Salvar</button>
-        </div>
+        <form id="form">
+            <div class="col-md-2">
+                <input type="text" class="form-control" id="nome" name="nome" placeholder="Sigla">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
+        </form>
     </div>
-
     <div id="listar">
         <table class="table" style="margin-top: 20px;">
             <thead>
@@ -25,10 +26,7 @@ require_once('cabecalho.php');
                     <td>1</td>
                     <td>Rio de Janeiro</td>
                 </tr>
-
             </tbody>
         </table>
     </div>
-
-
 </div>
