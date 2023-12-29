0c3cefbe589c240a67edb6c6a5c8d58a766ee291
João Corner <joaoeduardocorner@gmail.com>
2023-12-29 19:16:47 -0300

    salvando no bd

--- estados/salvar.php
+++ estados/salvar.php
@@ -1,4 +1,10 @@
 <?php
+require_once('../conexao.php');
+
 $nome = $_POST['nome'];
+
+$query = $pdo->prepare("INSERT INTO estados SET nome = :nome");
+$query->bindValue(':nome', $nome);
+$query->execute();
+
 echo 'Salvo com Sucesso';
-?>
