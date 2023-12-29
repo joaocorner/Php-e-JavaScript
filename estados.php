e6be0eb624c5b128c93fcf7382c9cf00c9103485
João Corner <joaoeduardocorner@gmail.com>
2023-12-28 22:48:55 -0300

    adicionado script ajax para insercao de dados

--- estados.php
+++ estados.php
@@ -30,3 +30,45 @@ require_once('cabecalho.php');
         </table>
     </div>
 </div>
+
+<!-- Script ajax para inserção de dados, isso é, não vai ter refresh na página -->
+<script type="text/javascript">
+    // o $ com o nome do componente significa que está tentando via jquery e não javascript chamar e dar referencia a um objeto que tenha esse id
+    $("#form").submit(function() {
+
+        // o preventDefault() é para não dar refresh na página
+        event.preventDefault();
+        // formData é um objeto que vai pegar todos os dados do formulário
+        var formData = new FormData(this);
+
+        // esse ajax pega os dados (formData) e envia para o caminho da url (estados.php) via post
+        $.ajax({
+            url: 'estados/salvar.php',
+            type: 'POST',
+            data: formData,
+
+            success: function(mensagem) {
+                $('#mensagem').text('');
+                $('#mensagem').removeClass()
+                if (mensagem.trim() == "Salvo com Sucesso") {
+
+                    $('#btn-fechar').click();
+                    listar();
+
+                } else {
+
+                    $('#mensagem').addClass('text-danger')
+                    $('#mensagem').text(mensagem)
+                }
+
+
+            },
+
+            cache: false,
+            contentType: false,
+            processData: false,
+
+        });
+
+    });
+</script>
