2dfb6845e7b41db5429636b66d18a20c669d17e8
João Corner <joaoeduardocorner@gmail.com>
2023-12-28 22:17:39 -0300

    inserindo campos de input e button

---
+++ estados.php
@@ -0,0 +1,14 @@
+<?php
+require_once('cabecalho.php');
+?>
+
+<div class="container" style="background: #f5f2f2; padding: 10px">
+    <div class="row">
+        <div class="col-md-2">
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Sigla">
+        </div>
+        <div class="col-md-2">
+            <button class="btn btn-primary">Salvar</button>
+        </div>
+    </div>
+</div>
